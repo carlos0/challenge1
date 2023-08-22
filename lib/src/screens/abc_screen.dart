341983ed1874198e2f3eee58588e325d705027fb
Carlos Enrique Macuchapi Parisaca <cmacuchapi@ine.com.bo>
2023-08-22 16:50:17 -0400

    functions add and reduce

--- lib/src/screens/abc_screen.dart
+++ lib/src/screens/abc_screen.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 
-class AbcScreen extends StatelessWidget {
+class AbcScreen extends StatefulWidget {
   const AbcScreen({super.key});
 
   @override
+  State<AbcScreen> createState() => _AbcScreenState();
+}
+
+class _AbcScreenState extends State<AbcScreen> {
+  String letra = "A";
+  int contador = 65;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: const Text("Challenge #1"), centerTitle: true,),
+      appBar: AppBar(
+        title: const Text("Challenge #1"),
+        centerTitle: true,
+      ),
       body: body(),
       floatingActionButton: crearFABs(),
     );
   }
 
   Widget body() {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("A", style: const TextStyle(fontSize: 70))
-        ],
+        children: [Text(letra, style: const TextStyle(fontSize: 150, fontWeight: FontWeight.bold))],
       ),
     );
   }
@@ -28,17 +36,37 @@ class AbcScreen extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         FloatingActionButton(
-          onPressed: () {},
+          onPressed: reducir,
           child: const Icon(Icons.arrow_back_ios),
         ),
         const SizedBox(
-          width: 10,
+          width: 20,
         ),
         FloatingActionButton(
-          onPressed: () {},
+          onPressed: adicionar,
           child: const Icon(Icons.arrow_forward_ios),
         ),
       ],
     );
   }
+
+  void adicionar() => setState(() {
+        if (contador == 90) {
+          letra = String.fromCharCode(65);
+          contador = 65;
+        } else {
+          letra = String.fromCharCode(contador + 1);
+          contador++;
+        }
+      });
+
+  void reducir() => setState(() {
+        if (contador == 65) {
+          letra = String.fromCharCode(90);
+          contador = 90;
+        } else {
+          letra = String.fromCharCode(contador - 1);
+          contador--;
+        }
+      });
 }
